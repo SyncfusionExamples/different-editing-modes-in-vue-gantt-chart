2a8e8ce1dc2ad91bc1f2188c4107216a546a5e7c
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-04-13 17:28:23 +0530

    Add Vue Gantt Editing sample
    Add Vue Gantt Editing sample

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# different-editing-modes-in-vue-gantt-chart
+# Different Editing Modes in Vue Gantt Chart Component
+
 A quick-start project that shows you how to enable editing in the Vue Gantt Chart. This project contains code snippets for performing cell, dialog, taskbar, and dependency editing in the component. Gantt component features are segregated into individual feature-wise modules. To use editing feature, inject the Edit module.
+
+Refer to the following editing documentation for the Syncfusion Vue Gantt Chart component: 
+https://ej2.syncfusion.com/vue/documentation/gantt/managing-tasks/managing-tasks 
+
+Check out this online example of editing in the Angular Gantt Chart component:
+https://ej2.syncfusion.com/vue/demos/#/material/gantt/editing.html 
+
+Refer to the following Syncfusion Vue Gantt Chart getting started video:
+https://www.youtube.com/watch?v=S1GbWmVcre0
+ 
+## Project prerequisites
+
+Make sure that you have the latest versions of Vue, Node, Vue Class Component, and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `different-editing-modes-in-vue-gantt-chart` repository and then open it in Visual Studio Code. Now, simply install all the necessary react packages into your current project using the `npm install` command and run your project using the `npm run serve` command.
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <ejs-gantt :dataSource="data"
+             :taskFields="taskFields"
+             :editSettings="editOptions"
+             :toolbar="toolbarOptions"
+             :treeColumnIndex="1"
+             :allowSelection="true"
+             :height="450">
+    <e-columns>
+      <e-column field="TaskID" headerText="Task ID" width="100" textAlign="Right"></e-column>
+      <e-column field="TaskName" headerText="Task Name" textAlign="Left" width="200"></e-column>
+      <e-column field="StartDate" headerText="Start Date" textAlign="Right" format="dd/MM/yyyy" width="120"></e-column>
+      <e-column field="Duration" headerText="Duration" textAlign="Right" width="120"></e-column>
+    </e-columns>
+  </ejs-gantt>
+</template>
+
+<script>
+import {GanttComponent, ColumnsDirective, ColumnDirective, Edit, Toolbar, Selection} from '@syncfusion/ej2-vue-gantt'
+import {projectNewData} from './data.js'
+export default {
+  name: 'App',
+  components: {
+    'ejs-gantt': GanttComponent,
+    'e-columns': ColumnsDirective,
+    'e-column': ColumnDirective
+  },
+  provide:{
+    gantt: [Edit, Toolbar, Selection]
+  },
+  data(){
+    return{
+      data: projectNewData,
+      editOptions:{
+        allowEditing: true,
+        allowAdding: true,
+        allowDeleting: true,
+        allowTaskbarEditing: true,
+        mode: 'Auto'
+      },
+      toolbarOptions: ['Add', 'Edit', 'Update', 'Delete', 'Cancel', 'ExpandAll', 'CollapseAll',
+                      'Indent', 'Outdent'],
+      taskFields:{
+        id: 'TaskID',
+        name: 'TaskName',
+        startDate: 'StartDate',
+        endDate: 'EndDate',
+        duration: 'Duration',
+        progress: 'Progress',
+        dependency: 'Predecessor',
+        child: 'subtasks'
+      },
+    }
+  }
+}
+</script>
+
+<style>
+@import url("https://cdn.syncfusion.com/ej2/material.css");
+</style>
